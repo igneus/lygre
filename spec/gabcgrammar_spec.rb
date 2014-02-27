e892c2a2db06cbb2f4f10f558cc8f223dcead333
Jakub Pavlík jn <severus@post.cz>
2014-02-27 20:32:09 +0100

    specs: readability matters

--- spec/gabcgrammar_spec.rb
+++ spec/gabcgrammar_spec.rb
@@ -1,21 +1,19 @@
 require_relative 'spec_helper'
 
-describe GabcParser do
-
-  before :all do
-    @parser = GabcParser.new
-  end
+# the GabcParser is described through the file format
+# it should be able to parse
+describe 'gabc' do
 
   describe 'valid gabc file must contain a header delimiter' do
 
     it 'file with just the header delimiter is valid' do
       src = "%%\n"
-      @parser.parse(src).should compile
+      src.should compile
     end
 
     it 'file without header delimiter is invalid' do
       src = ""
-      @parser.parse(src).should_not compile
+      src.should_not compile
     end
   end
 
@@ -24,46 +22,57 @@ describe GabcParser do
     describe 'whitespace' do
       it '- any number of empty lines' do
         src = "\n\n\n\n%%\n"
-        @parser.parse(src).should compile
+        src.should compile
       end
 
       it '- spaces and tabs' do
         src = "    \n\t\n%%\n"
-        @parser.parse(src).should compile
+        src.should compile
       end
     end
 
     describe 'header fields' do
       it '- e.g. "name"' do
         src = "name: incipit;\n%%\n"
-        @parser.parse(src).should compile
+        src.should compile
       end
 
       it 'whose identifiers may include dashes' do
         src = "office-part: introitus/...;\n%%\n"
-        @parser.parse(src).should compile
+        src.should compile
       end
 
       it '- a lot of them' do
         src = load_example 'header.gabc'
-        @parser.parse(src).should compile
+        src.should compile
       end
     end
 
     describe 'comments' do
       it ', which may occupy a whole line' do
         src = "% comment\n%%\n\n"
-        @parser.parse(src).should compile
+        src.should compile
       end
 
       it 'following whitespace' do
         src = "  % comm' comm' comment\n%%\n"
-        @parser.parse(src).should compile
+        src.should compile
       end
 
       it 'following header fields' do
         src = "name: incipit; % what a beautiful name!\n%%\n"
-        @parser.parse(src).should compile
+        src.should compile
+      end
+    end
+
+  end
+
+  describe 'body' do
+
+    describe 'clef' do
+      it 'may be c clef on the 3rd line' do
+        src = "%%\n(c3)"
+        src.should compile
       end
     end
 
