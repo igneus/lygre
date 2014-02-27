8af4b0e8e7a75e5fec54e1f8f76bd6037e338a8c
Jakub Pavlík jn <severus@post.cz>
2014-02-27 19:34:52 +0100

    grely executable at least performs a 'validation' of a specified file

--- bin/grely.rb
+++ bin/grely.rb
@@ -2,7 +2,11 @@
 
 # grely
 
+# one day it will perform
 # conversion gregorio (gabc) -> lilypond
+
+# now it only
+# says if it is able to parse the given input file 
 
 require 'polyglot'
 require 'treetop'
@@ -10,6 +14,18 @@ require 'treetop'
 Treetop.load File.expand_path('../lib/lygre/gabcgrammar', File.dirname(__FILE__))
 
 parser = GabcParser.new
-src = "%%
-(c3)"
-puts parser.parse(src)
+
+if ARGV.size >= 1 then
+  inputf = ARGV[0]
+  rf = File.open inputf
+else
+  rf = STDIN
+end
+
+input = rf.read
+
+if parser.parse(input) then
+  puts 'grely thinks this is a valid gabc file.'
+else
+  puts 'grely thinks the input is not valid gabc.'
+end
