878d90bd40756603b817a71c55f44633b5e4f18a
Jakub Pavlík <severus@post.cz>
2016-01-01 17:12:10 +0100

    divisio with linebreak

--- lib/lygre/gabcsemantics.rb
+++ lib/lygre/gabcsemantics.rb
@@ -113,7 +113,7 @@ module Gabc
           end
         elsif ele.is_a? DivisioNode then
           arr << GabcDivisio.new do |d|
-            d.type = ele.text_value
+            d.type = ele.text_value.gsub(/\w+/, '') # remove eventual line-breaks etc.
           end
         else
           collect_notes ele, arr
