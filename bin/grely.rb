07c3b896b1156fbbdbf8258b1eb86c7cb7986427
Jakub Pavlík <severus@post.cz>
2016-01-01 16:16:35 +0100

    grely: accept any number of commandline arguments

--- bin/grely.rb
+++ bin/grely.rb
@@ -6,27 +6,34 @@
 
 require 'grely'
 
-parser = GabcParser.new
+def grely(rf)
+  input = rf.read
 
-if ARGV.size >= 1 then
-  inputf = ARGV[0]
-  rf = File.open inputf
-else
-  rf = STDIN
+  parser = GabcParser.new
+  result = parser.parse(input)
+
+  if result then
+    puts LilypondConvertor.new(cadenza: true).convert result.create_score
+    return true
+  else
+    STDERR.puts 'grely considers the input invalid gabc:'
+    STDERR.puts
+    STDERR.puts "'#{parser.failure_reason}' on line #{parser.failure_line} column #{parser.failure_column}:"
+    STDERR.puts input.split("\n")[parser.failure_line-1]
+    STDERR.puts (" " * parser.failure_column) + "^"
+    return false
+  end
 end
 
-input = rf.read
-
-result = parser.parse(input)
-
-if result then
-  puts LilypondConvertor.new(cadenza: true).convert result.create_score
-  exit 0
+ok = true
+if ARGV.size < 1 then
+  grely STDIN
 else
-  STDERR.puts 'grely considers the input invalid gabc:'
-  STDERR.puts 
-  STDERR.puts "'#{parser.failure_reason}' on line #{parser.failure_line} column #{parser.failure_column}:"
-  STDERR.puts input.split("\n")[parser.failure_line-1]
-  STDERR.puts (" " * parser.failure_column) + "^"
-  exit 1
+  ARGV.each do |f|
+    File.open(f) do |rf|
+      ok &&= grely rf
+    end
+  end
 end
+
+exit ok ? 0 : 1
