d8a57d9df24ddb8e9dc4332b2301aad100d96791
Jakub Pavlik <severus@post.cz>
2022-10-12 20:35:02 +0200

    all divisiones listed in the documentation

--- spec/gabcparser_spec.rb
+++ spec/gabcparser_spec.rb
@@ -143,15 +143,15 @@ ec(hihi)ce(e.) Dó(e.f!gwh/hi)mi(h)nus(h) vé(hi)ni(ig/ih)et.(h.) (::)"
       ['(h_[oh:ol])'],
 
       # divisiones / bars
-      #['(‘)', 'virgula'],
-      #['(‘0)', 'virgula on the ledger line above the staff'],
-      #['(^)', 'divisio “minimis” (eighth bar)'],
-      #['(^0)', 'once again divisio “minimis”'],
+      ['(‘)', 'virgula'],
+      ['(‘0)', 'virgula on the ledger line above the staff'],
+      ['(^)', 'divisio “minimis” (eighth bar)'],
+      ['(^0)', 'once again divisio “minimis”'],
       ['(,)', 'divisio minima (quarter bar)'],
-      #['(,0)', 'divisio minima on the ledger line above the staff'],
+      ['(,0)', 'divisio minima on the ledger line above the staff'],
       ['(;)',  'divisio minor (half bar)'],
       ['(:)', 'divisio maior (full bar)'],
-      #['(:?)', 'dotted divisio maior'],
+      ['(:?)', 'dotted divisio maior'],
       ['(::)', 'divisio finalis'],
       ['(;1)', 'Dominican bars:'],
       ['(;2)'],
@@ -159,10 +159,10 @@ ec(hihi)ce(e.) Dó(e.f!gwh/hi)mi(h)nus(h) vé(hi)ni(ig/ih)et.(h.) (::)"
       ['(;4)'],
       ['(;5)'],
       ['(;6)'],
-      #['(;7)'],
-      #['(;8)'],
+      ['(;7)'],
+      ['(;8)'],
       ["(:')", 'divisio (maior in this case) with vertical episema'],
-      ["(:')", 'divisio (maior in this case) with bar brace'],
+      ["(:_)", 'divisio (maior in this case) with bar brace'],
     ].each do |gabc, label|
       it(label || gabc) do
         rparse(gabc).should be_truthy
